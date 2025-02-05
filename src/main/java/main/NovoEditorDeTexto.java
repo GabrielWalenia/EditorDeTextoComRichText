46c865e1eca2f2d7d43f063874ffad7326a54642
GabrielWalenia <gabriel.walenia@gmail.com>
2025-02-05 11:26:49 -0300

    first commit

---
+++ src/main/java/main/NovoEditorDeTexto.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main;
+
+import main.janelas.JanelaInicial;
+
+/**
+ *
+ * @author gabri
+ */
+public class NovoEditorDeTexto {
+    public static void main(String[] args){
+        JanelaInicial editorDeTexto = new JanelaInicial();
+        editorDeTexto.iniciar();
+    }
+}
---
+++ src/main/java/texto/Texto.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package texto;
+
+/**
+ *
+ * @author gabri
+ */
+public class Texto {
+    private String titulo;
+    private String conteudo;
+    
+    public String getTitulo(){
+        return titulo;
+    }
+    
+    public void setTitulo(String titulo){
+        this.titulo = titulo;
+    }
+    public String getConteudo(){
+        return conteudo;
+    }
+    public void setConteudo(String conteudo){
+        this.conteudo = conteudo;
+    }
+}
